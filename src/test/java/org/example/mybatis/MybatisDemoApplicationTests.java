63392592fa004ec3f4b53d19e2e2c31e588ae282
liny <liny@example.com>
2026-08-20 15:38:44 +0800

    test: 新增 SimpleQuery 的測試

--- src/test/java/org/example/mybatis/MybatisDemoApplicationTests.java
+++ src/test/java/org/example/mybatis/MybatisDemoApplicationTests.java
@@ -1,6 +1,9 @@
 package org.example.mybatis;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
+import com.baomidou.mybatisplus.extension.injector.methods.AlwaysUpdateSomeColumnById;
+import com.baomidou.mybatisplus.extension.toolkit.SimpleQuery;
 import org.example.mybatis.adapter.mapper.UserMapper;
 import org.example.mybatis.domain.User;
 import org.example.mybatis.domain.UserRepository;
@@ -9,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -26,7 +30,6 @@ class MybatisDemoApplicationTests {
         List<User> users = userMapper.selectList(null);
         assertNotNull(users);
         assertEquals(5, users.size());
-        users.forEach(IO::println);
     }
 
     @Test
@@ -48,5 +51,15 @@ class MybatisDemoApplicationTests {
         User user = userMapper.selectOne(new QueryWrapper<User>().eq("id", 2));
         assertNotNull(user);
         assertEquals(2, user.getId());
+    }
+
+    @Test
+    void testSimpleQuery() {
+        Map<Long, User> userMap = SimpleQuery.keyMap(Wrappers.lambdaQuery(User.class).ge(User::getId, 3),
+                User::getId,  // 這是 map key
+                user -> IO.println(user.getName()));
+        assertNotNull(userMap);
+        assertEquals(3, userMap.size()); // User ID >= 3 的有 3 筆。
+        assertTrue(userMap.containsKey(4L));
     }
 }
